4537eb5262d5831678b242f51baefe1899d7b735
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-09-27 01:24:52 +0200

    Algo ✅

---
+++ Java/first.java
@@ -0,0 +1,8 @@
+// In this challenge, you are to search an array to chech if it has a partucular number in it.
+
+public class first {
+    public static void  main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
