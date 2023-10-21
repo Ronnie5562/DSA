04aa52b0131806b70115007cd0956a88c3ddd426
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-21 16:32:44 +0200

    Binary search

---
+++ Java/BinarySearch/OrderAgnosticBinarySearch.java
@@ -0,0 +1,30 @@
+package BinarySearch;
+
+public class OrderAgnosticBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-27, -5, 0, 2, 4, 5, 6, 8, 12, 15, 45, 67, 84, 99, 200, 567};
+        int target = 15;
+        int result = orderAgnosticBinarySearch(arr, target);
+        System.out.println(result);
+    }
+
+    static int orderAgnosticBinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
