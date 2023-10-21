d6cab30bd095c1d131d79e1e347efc10cf151bec
Abimbola Ronald <abimbolaaderinsola212@gmail.com>
2023-10-21 15:41:28 +0200

    Binary search

---
+++ Java/BinarySearch/BinarySearch.java
@@ -0,0 +1,32 @@
+package BinarySearch;
+
+import javax.xml.transform.stream.StreamSource;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-27, -5, 0, 2, 4, 5, 6, 8, 12, 15, 45, 67, 84, 99, 200, 567};
+        int target = 15;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find the middle element
+
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
